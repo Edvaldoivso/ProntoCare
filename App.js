63d50e5af30f0ee5b40ec8f1a2843a926b974b60
Edvaldo <you@example.com>
2020-11-28 18:10:29 -0300

    Alterações do Sabado

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 const express = require("express");
 const bodyParser = require("body-parser")
-const handlebars = require ("express-handlebars")
+const handlebars = require("express-handlebars")
 const DadosProntuario = require("./Models/InsertProntuario")
 const Handlebars = require('handlebars')
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
 const InsertVacinas = require("./Models/InsertVacinas")
 const InsertConsultas = require("./Models/InsertConsultas")
 const DadosAlergia = require("./Models/InsertAlergias")
@@ -26,171 +26,151 @@ const app = express();
 
 // parse application/x-www-form-urlencoded--Incluido por causa que era assim !
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+
 // parse application/json
 app.use(bodyParser.json())
 
 
-//app.engine("handlebars", handlebars({defaultLayout:'main'})) Antigo que dava erro
-
 app.engine('handlebars', handlebars({//Novo substituiu o inicial e da certo
     defaultLayout: 'main',
-     handlebars: allowInsecurePrototypeAccess(Handlebars)
-  }));
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+}));
 
-app.set('view engine','handlebars')
+
+app.set('view engine', 'handlebars')
 
 
 //Arquivos Staticos
 
 
-app.use(express.static(path.join(__dirname,"img")))
-app.use(express.static(path.join(__dirname,"css")))
-app.use(express.static(path.join(__dirname,"js")))
-
-  
-/*
-//Rotas do handlebars
-app.get("/Index", function(req, res){    
-    res.render("Index");
-  }),
-
-*/
+app.use(express.static(path.join(__dirname, "img")))
+app.use(express.static(path.join(__dirname, "css")))
+app.use(express.static(path.join(__dirname, "js")))
 
 
-app.get("/Index", function(req, res){   
+
+
+app.get("/Index", function (req, res) {
 
     res.render("Index");
-   
-  }),
+
+}),
 
 
 
 
-
-app.post('/Index2',function(req,res){
-//res.send("Senha:" + req.body.senha + "Aqui o dado </br>" + "CPF:" + req.body.CPF)
-InsertLogin.create({
-    cpf:req.body.CPF,
-    senha:req.body.senha
-    
-    })
-
-
-})
-
-
-app.post('/Login', function(req,res){
+app.post('/Login', function (req, res) {
 
     InsertLogin.findOne({
-     where:{
-        senha: req.body.senha,
-        cpf:req.body.CPF           
-    }
- }).then(function(InsertLogin){
-     if(InsertLogin){
-        res.render("Home")
-     }else{
-        //res.render("Index");        
-         //var mensagem = InsertLogin=InsertLogin?InsertLogin:"usuario";
-         //console.log("Não Encontrei o "+InsertLogin)
-         //return mensagem;
-         res.render("Erro") 
-     }
-     
- }).catch(function(err){
- console.log("Não Encontrei o "+InsertLogin + "tive um erro: "+err)
 
- })
+        
+        where: {
+            senha: req.body.senha,
+            cpf: req.body.CPF
+        }
+    }).then(function (InsertLogin) {
+        if (InsertLogin) {
+            res.render("Home")
+        } else {
+            //res.render("Index");        
+            //var mensagem = InsertLogin=InsertLogin?InsertLogin:"usuario";
+            //console.log("Não Encontrei o "+InsertLogin)
+            //return mensagem;
+            res.render("Erro")
+        }
+
+    }).catch(function (err) {
+        console.log("Não Encontrei o " + InsertLogin + "tive um erro: " + err)
+
+    })
 
 
 })
 
-app.get("/Vacinas", function(req, res){
+app.get("/Vacinas", function (req, res) {
     //res.render("Vacinas");
     InsertVacinas.findOne({
-        where:{
-            //senha: req.body.senha,
-            id:2, 
+        where: {
+            id: 2,
         },
     }
-    ).then(function(InsertVacinas){
-        res.render('Vacinas',{InsertVacinas:InsertVacinas})
+    ).then(function (InsertVacinas) {
+        res.render('Vacinas', { InsertVacinas: InsertVacinas })
     })
-   
+
 }),
 
 
-app.get("/Consultas", function(req, res){
-    //res.render("Consultas")
-    
-    
-    InsertConsultas.findOne({
-        where:{
-            //senha: req.body.senha,
-            id:1
-        },
-    }
-    ).then(function(DadosConsulta){
-    res.render('Consultas', {DadosConsulta:DadosConsulta})
+    app.get("/Consultas", function (req, res) {
+        //res.render("Consultas")
+
+
+        InsertConsultas.findOne({
+            where: {
+                //senha: req.body.senha,
+                id: 1
+            },
+        }
+        ).then(function (DadosConsulta) {
+            res.render('Consultas', { DadosConsulta: DadosConsulta })
+
+        })
+
+
+    }),
+
+
+    app.get("/Home", function (req, res) {
+        res.render("Home")
 
     })
-   
-   
-}),
-
-
-app.get("/Home", function(req, res){
-    res.render("Home")
-   
-})
 
 
 
-app.get("/Alertas", function(req, res){
+app.get("/Alertas", function (req, res) {
     res.render("Alertas")
-   
+
 })
 
 
 
-app.get("/Alergias", function(req, res){
+app.get("/Alergias", function (req, res) {
     //res.render("Alergias")
-    
+
 
     DadosAlergia.findOne({
-        where:{
+        where: {
             //senha: req.body.senha,
-            id:2, 
+            id: 2,
         },
     }
-    ).then(function(DadosAlergia){                   
-        res.render('Alergias',{DadosAlergia:DadosAlergia})
-    })    
-   
+    ).then(function (DadosAlergia) {
+        res.render('Alergias', { DadosAlergia: DadosAlergia })
+    })
+
 })
 
 
 
 
-app.post('/InsertAlergias',function(req,res){
-//res.send("Titulo:" + req.body.Descricao + "Aqui o Titulo </br>" + "CPF:" + req.body.Titulo)
+app.post('/InsertAlergias', function (req, res) {
+    //res.send("Titulo:" + req.body.Descricao + "Aqui o Titulo </br>" + "CPF:" + req.body.Titulo)
     DadosAlergia.create({
 
-            NomeAlergia:req.body.Titulo,
-            Descricao:req.body.Descricao,            
-            
-            
-    }).then(function(DadosAlergia){
-    if(DadosAlergia){
-        res.render("Sucesso")
-    }else{
-        res.send("Erro")    
+        NomeAlergia: req.body.Titulo,
+        Descricao: req.body.Descricao,
 
-    }
 
-    }).catch(function(err){
-        console.log("Tivemos um erro em" +DadosAlergia + err)
+    }).then(function (DadosAlergia) {
+        if (DadosAlergia) {
+            res.render("Sucesso")
+        } else {
+            res.send("Erro")
+
+        }
+
+    }).catch(function (err) {
+        console.log("Tivemos um erro em" + DadosAlergia + err)
     })
 
 })
@@ -199,66 +179,112 @@ app.post('/InsertAlergias',function(req,res){
 
 
 
-app.get("/Prontuario",function(req,res){
- 
+app.get("/Prontuario", function (req, res) {
+
     DadosProntuario.findOne({
-        where:{
+        where: {
             //senha: req.body.senha,
-            id:2, 
+            id: 2,
         },
     }
-    ).then(function(DadosProntuario){                   
-    res.render('Prontuario',{DadosProntuario:DadosProntuario})
-})
+    ).then(function (DadosProntuario) {
+        res.render('Prontuario', { DadosProntuario: DadosProntuario })
+    })
 
 
 });
 
 
 
-/*
-app.get("/IndexAtendimento", function(req, res){
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("/IndexAtendimento", function (req, res) {
+    //res.render("IndexAtendimento")
+
+    DadosProntuario.findAll({
+        where: {
+            nome: "TESTE"
+        }
+    }).then(function (DadosProntuario) {
+        res.render('IndexAtendimento', { DadosProntuario: DadosProntuario })
+       
+
+    }).catch(function (err) {
+        console.log("Tivemos um erro em" + err)
+    })
+
+
+})
+
+
+
+
+app.get("/DadosAtendimento/:id",async function(req, res) {
     //res.render("IndexAtendimento")
     
-    DadosProntuario.update({
+
+    await DadosProntuario.findOne({
+        
+        where: {
+           id: req.params.id
+        }
+    }).then(function (DadosProntuario) {
+        res.render('DadosAtendimento', { DadosProntuario: DadosProntuario })
+        console.log("Valor parametro= "+ req.params.id)
+       
+
+    }).catch(function (err) {
+        console.log("Tivemos um erro em" + err)
+    })
+
+
+})
+
+
+
+
+app.post("/IndexUpdate/:id",  function(req, res){
+    //res.render("IndexAtendimento")
     
-    Nome:"Teste",
-    CPF:45000000
+     DadosProntuario.update({
+        nome:'Novo Teste',   
+        Peso:req.body.Peso ,
+        Pressao:req.body.Pressao ,
+        Idade:req.body.Idade,
+        Rua:req.body.Rua ,
+        Bairro:req.body.Bairro ,
+        Cidade:req.body.Cidade ,
+        Cep:req.body.Cep ,
+        Complemento:req.body.Complemento ,
+        Numero:req.body.Numero ,
+        //TelefoneEmergencia:req.body.TelefoneEmergencia ,
+        email:req.body.email ,
+        TelefoneRecado:req.body.TelefoneRecado ,
+        emailOpcional:req.body.emailOpcional ,
+        NomeResponsavel:req.body.NomeResponsável ,
+        NomeCuidador:req.body.NomeCuidador ,
+        TelefoneCuidador:req.body.TelefoneCuidador
+
        
 },{
     where:{
-        id:1
+        id: req.params.id
     }
 }
 
 ).then(function(DadosProntuario){                   
-    res.render('IndexAtendimento',{DadosProntuario:DadosProntuario})
-}).catch(function(err){
-console.log("Tivemos um erro em" + err)
-})
-
-   
-})
-*/
-
-
-
-
-
-
-
-
-
-
-app.get("/IndexAtendimento", function(req, res){
-    //res.render("IndexAtendimento")
-    
-    DadosProntuario.findOne({
-        where:{
-            nome:"TESTE"
-        }
-}).then(function(DadosProntuario){                   
-    res.render('IndexAtendimento',{DadosProntuario:DadosProntuario})
+    res.render('DadosAtendimento',{DadosProntuario:DadosProntuario})
 }).catch(function(err){
 console.log("Tivemos um erro em" + err)
 })
@@ -271,33 +297,50 @@ console.log("Tivemos um erro em" + err)
 
 
 
-app.post('Update',function(req,res){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+app.post('Update', function (req, res) {
     //res.send("Titulo:" + req.body.Descricao + "Aqui o Titulo </br>" + "CPF:" + req.body.Titulo)
     DadosProntuario.findOne({
-            
-                   
-                 NomeAlergia:req.body.Titulo,
-                CPF:req.body.CPF,
-                
-                
-        }).then(function(DadosProntuario){
-        if(DadosProntuario){
+
+
+        NomeAlergia: req.body.Titulo,
+        CPF: req.body.CPF,
+
+
+    }).then(function (DadosProntuario) {
+        if (DadosProntuario) {
             res.render("Salvei Sucesso")
-        }else{
-            res.send("Erro")    
-    
+        } else {
+            res.send("Erro")
+
         }
-    
-        }).catch(function(err){
-            console.log("Tivemos um erro em" + DadosProntuario + err)
-        })
-    
+
+    }).catch(function (err) {
+        console.log("Tivemos um erro em" + DadosProntuario + err)
     })
 
+})
 
 
 
 
 
+*/
 
 app.listen(8081);
--- Models/InsertProntuario.js
+++ Models/InsertProntuario.js
@@ -5,228 +5,228 @@ const IndexDB = require("./Index");
 var DadosProntuario = db.sequelize.define('Prontuario', {
 
 
-    id:{
-        type:db.Sequelize.INTEGER,
-        field:'id',
-        allowNull:false,
-        autoIncrement:true,
-        primaryKey:true
-    
-    },    
-    
-   senha:{
-        type:db.Sequelize.STRING,
-        field:'senha'
-    },
-    
-    
-    Nome:{
-        type:db.Sequelize.STRING,
-        field:'nome'
-    },
-    CPF:{
-        type:db.Sequelize.STRING,
-        field:'cpf'
-    },
-    RG:{
-        type:db.Sequelize.STRING,
-        field:'RG'
-    },
-    Idade:{
-        type:db.Sequelize.FLOAT,
-        field:'idade'
-    },
-    Fenotipo:{
-        type:db.Sequelize.FLOAT,
-        field:'fenotipo'
-    },
-    Peso:{
-        type:db.Sequelize.FLOAT,
-        field:'Peso'
-    },
-    Altura:{
-        type:db.Sequelize.FLOAT,
-        field:'Altura'
-    },
-    SexoBiologico:{
-        type:db.Sequelize.FLOAT,
-        field:'SexoBiologico'
-    },
-    Genero:{
-        type:db.Sequelize.FLOAT,
-        field:'Genero'
-    },
-    GrupoRisco:{
-        type:db.Sequelize.FLOAT,
-        field:'GrupoRisco'
-    },
-    DisturbioPsquicos:{
-        type:db.Sequelize.FLOAT,
-        field:'DisturbioPsquicos'
-    },
-    PCD:{
-        type:db.Sequelize.FLOAT,
-        field:'PCD'
-    },
-    GrSanguineo:{
-        type:db.Sequelize.STRING,
-        field:'Gr Sanguineo'
-    },
-    Pressao:{
-        type:db.Sequelize.FLOAT,
-        field:'Pressao'
-    },
-    Doador:{
-        type:db.Sequelize.FLOAT,
-        field:'Doador'
-    },Rua :{
-        type:db.Sequelize.STRING,
-        field:'Rua'
-    },
-    Bairro:{
-        type:db.Sequelize.STRING,
-        field:'Bairro'
-    },
-    Cidade:{
-        type:db.Sequelize.STRING,
-        field:'Cidade'
-    },
-    Cep:{
-        type:db.Sequelize.STRING,
-        field:'Cep'
-    },
-    Complemento:{
-        type:db.Sequelize.STRING,
-        field:'Complemento'
-    },
-    Numero:{
-        type:db.Sequelize.STRING,
-        field:'Numero'
-    },
-    TelefoneEmergencia:{
-        type:db.Sequelize.STRING,
-        field:'TelefoneEmergencia'
-    },  
-    email:{
-        type:db.Sequelize.STRING,
-        field:'email'
-    },  
-    TelefoneRecado:{
-        type:db.Sequelize.STRING,
-        field:'TelefoneRecado'
-    },  
-    emailOpcional:{
-        type:db.Sequelize.STRING,
-        field:'emailOpcional'
-    },  
-    TelefoneCuidador:{
-        type:db.Sequelize.STRING,
-        field:'TelefoneCuidador'
-    },  
-    NomeResponsavel:{
-        type:db.Sequelize.STRING,
-        field:'NomeResponsavel'
-    },
-    NomeCuidador:{
-        type:db.Sequelize.STRING,
-        field:'NomeCuidador'
+    id: {
+        type: db.Sequelize.INTEGER,
+        field: 'id',
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+
     },
 
-    NomeAlergia:{
-        type:db.Sequelize.STRING,
-        field:'NomeAlergia'
-    },
-    
-    Descricao:{
-        type:db.Sequelize.STRING,
-        field:'Descricao'
-    },
-    Homologador:{
-        type:db.Sequelize.STRING,
-        field:'Homologador'
-    },
-    DataInclusao:{
-        type:db.Sequelize.DATEONLY,
-        field:'DataInclusao'
-    },
-    Consulta:{
-        type:db.Sequelize.STRING,
-        field:'Consultas'
-    },
-    Data:{
-        type:db.Sequelize.DATEONLY,
-        field:'Data'
-    },
-    Especialidade:{
-    type:db.Sequelize.STRING,
-    field:'Especialidade'
-    
-    },
-    Medico:{
-    type:db.Sequelize.STRING,
-    field:'Medico'
-    },
-    NomeHospital:{
-        type:db.Sequelize.STRING,
-        field:'NomeHospital'
-    },
-    
-    RuaHospital:{
-        type:db.Sequelize.STRING,
-        field:'RuaHospital'
-    },
-    
-    NumeroHospital:{
-        type:db.Sequelize.STRING,
-        field:'NumeroHospital'
-    },
-    
-    BairroHospital:{
-        type:db.Sequelize.STRING,
-        field:'BairroHospital'
-    },
-    
-    CepHospital:{
-        type:db.Sequelize.STRING,
-        field:'CepHospital'
-    },
-    
-    TelefoneHospital:{
-        type:db.Sequelize.STRING,
-        field:'TelefoneHospital'
-    },
-    Rua :{
-        type:db.Sequelize.STRING,
-        field:'Rua'
-    },
-    Bairro:{
-        type:db.Sequelize.STRING,
-        field:'Bairro'
-    },
-    Cidade:{
-        type:db.Sequelize.STRING,
-        field:'Cidade'
-    },
-    Cep:{
-        type:db.Sequelize.STRING,
-        field:'Cep'
-    },
-    Complemento:{
-        type:db.Sequelize.STRING,
-        field:'Complemento'
-    },
-    Numero:{
-        type:db.Sequelize.STRING,
-        field:'Numero'
+    senha: {
+        type: db.Sequelize.STRING,
+        field: 'senha'
     },
 
-    Vacina :{
-        type:db.Sequelize.STRING,
-        field:'vacina'
+
+    Nome: {
+        type: db.Sequelize.STRING,
+        field: 'nome'
     },
-    Validade:{
-        type:db.Sequelize.DATE,
-        field:'Date'
+    CPF: {
+        type: db.Sequelize.STRING,
+        field: 'cpf'
+    },
+    RG: {
+        type: db.Sequelize.STRING,
+        field: 'RG'
+    },
+    Idade: {
+        type: db.Sequelize.FLOAT,
+        field: 'idade'
+    },
+    Fenotipo: {
+        type: db.Sequelize.FLOAT,
+        field: 'fenotipo'
+    },
+    Peso: {
+        type: db.Sequelize.FLOAT,
+        field: 'Peso'
+    },
+    Altura: {
+        type: db.Sequelize.FLOAT,
+        field: 'Altura'
+    },
+    SexoBiologico: {
+        type: db.Sequelize.FLOAT,
+        field: 'SexoBiologico'
+    },
+    Genero: {
+        type: db.Sequelize.FLOAT,
+        field: 'Genero'
+    },
+    GrupoRisco: {
+        type: db.Sequelize.FLOAT,
+        field: 'GrupoRisco'
+    },
+    DisturbioPsquicos: {
+        type: db.Sequelize.FLOAT,
+        field: 'DisturbioPsquicos'
+    },
+    PCD: {
+        type: db.Sequelize.FLOAT,
+        field: 'PCD'
+    },
+    GrSanguineo: {
+        type: db.Sequelize.STRING,
+        field: 'Gr Sanguineo'
+    },
+    Pressao: {
+        type: db.Sequelize.FLOAT,
+        field: 'Pressao'
+    },
+    Doador: {
+        type: db.Sequelize.FLOAT,
+        field: 'Doador'
+    }, Rua: {
+        type: db.Sequelize.STRING,
+        field: 'Rua'
+    },
+    Bairro: {
+        type: db.Sequelize.STRING,
+        field: 'Bairro'
+    },
+    Cidade: {
+        type: db.Sequelize.STRING,
+        field: 'Cidade'
+    },
+    Cep: {
+        type: db.Sequelize.STRING,
+        field: 'Cep'
+    },
+    Complemento: {
+        type: db.Sequelize.STRING,
+        field: 'Complemento'
+    },
+    Numero: {
+        type: db.Sequelize.STRING,
+        field: 'Numero'
+    },
+    TelefoneEmergencia: {
+        type: db.Sequelize.STRING,
+        field: 'TelefoneEmergencia'
+    },
+    email: {
+        type: db.Sequelize.STRING,
+        field: 'email'
+    },
+    TelefoneRecado: {
+        type: db.Sequelize.STRING,
+        field: 'TelefoneRecado'
+    },
+    emailOpcional: {
+        type: db.Sequelize.STRING,
+        field: 'emailOpcional'
+    },
+    TelefoneCuidador: {
+        type: db.Sequelize.STRING,
+        field: 'TelefoneCuidador'
+    },
+    NomeResponsavel: {
+        type: db.Sequelize.STRING,
+        field: 'NomeResponsavel'
+    },
+    NomeCuidador: {
+        type: db.Sequelize.STRING,
+        field: 'NomeCuidador'
+    },
+
+    NomeAlergia: {
+        type: db.Sequelize.STRING,
+        field: 'NomeAlergia'
+    },
+
+    Descricao: {
+        type: db.Sequelize.STRING,
+        field: 'Descricao'
+    },
+    Homologador: {
+        type: db.Sequelize.STRING,
+        field: 'Homologador'
+    },
+    DataInclusao: {
+        type: db.Sequelize.DATEONLY,
+        field: 'DataInclusao'
+    },
+    Consulta: {
+        type: db.Sequelize.STRING,
+        field: 'Consultas'
+    },
+    Data: {
+        type: db.Sequelize.DATEONLY,
+        field: 'Data'
+    },
+    Especialidade: {
+        type: db.Sequelize.STRING,
+        field: 'Especialidade'
+
+    },
+    Medico: {
+        type: db.Sequelize.STRING,
+        field: 'Medico'
+    },
+    NomeHospital: {
+        type: db.Sequelize.STRING,
+        field: 'NomeHospital'
+    },
+
+    RuaHospital: {
+        type: db.Sequelize.STRING,
+        field: 'RuaHospital'
+    },
+
+    NumeroHospital: {
+        type: db.Sequelize.STRING,
+        field: 'NumeroHospital'
+    },
+
+    BairroHospital: {
+        type: db.Sequelize.STRING,
+        field: 'BairroHospital'
+    },
+
+    CepHospital: {
+        type: db.Sequelize.STRING,
+        field: 'CepHospital'
+    },
+
+    TelefoneHospital: {
+        type: db.Sequelize.STRING,
+        field: 'TelefoneHospital'
+    },
+    Rua: {
+        type: db.Sequelize.STRING,
+        field: 'Rua'
+    },
+    Bairro: {
+        type: db.Sequelize.STRING,
+        field: 'Bairro'
+    },
+    Cidade: {
+        type: db.Sequelize.STRING,
+        field: 'Cidade'
+    },
+    Cep: {
+        type: db.Sequelize.STRING,
+        field: 'Cep'
+    },
+    Complemento: {
+        type: db.Sequelize.STRING,
+        field: 'Complemento'
+    },
+    Numero: {
+        type: db.Sequelize.STRING,
+        field: 'Numero'
+    },
+
+    Vacina: {
+        type: db.Sequelize.STRING,
+        field: 'vacina'
+    },
+    Validade: {
+        type: db.Sequelize.DATE,
+        field: 'Date'
     }
 
 
@@ -235,16 +235,17 @@ var DadosProntuario = db.sequelize.define('Prontuario', {
 })
 
 
-DadosProntuario.belongsTo(IndexDB, {as:'Index',
-foreingKey:'id',
-onUpdate:'CASCADE'
+DadosProntuario.belongsTo(IndexDB, {
+    as: 'Index',
+    foreingKey: 'id',
+    onUpdate: 'CASCADE'
 })
 
 //DadosProntuario.sync({force:true});//Esta linha serve para criar a tabela
 
 
 
-   
+
 /*
 DadosProntuario.create({
     
@@ -290,5 +291,5 @@ DadosProntuario.create({
 
 
 module.exports = DadosProntuario;
-    
+
 
